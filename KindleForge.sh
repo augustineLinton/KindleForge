ae9480bc538ee5039491b919aac66a7eba2b047f
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2025-06-14 20:44:49 +0100

    Attempt To Revert To Utild Standard
    Did this even change anything??

--- KindleForge.sh
+++ KindleForge.sh
@@ -11,25 +11,28 @@ TARGET_DIR="/var/local/mesquite/KindleForge"
 DB="/var/local/appreg.db"
 APP_ID="xyz.penguins184.kindleforge"
 
-#Copy UTILD
+#DO NOT MODIFY BELOW
+
+#Copy UTILD Binaries To VAR/LOCAL/KMC
 BINARY="$SOURCE_DIR/.illusion/UtildSF"
 [ -e /lib/ld-linux-armhf.so.3 ] && BINARY="$SOURCE_DIR/.illusion/UtildHF"
+
 [ -e "$BINARY" ] && cp "$BINARY" /var/local/kmc && rm "$BINARY"
 
 #Run UTILD
 if [ -e /lib/ld-linux-armhf.so.3 ]; then
-    rm -f /var/local/kmc/UtildSF #Just In Case!
+    rm -f /var/local/kmc/UtildSF
 
     chmod +x /var/local/kmc/UtildHF
     /var/local/kmc/UtildHF
 else
-    rm -f /var/local/kmc/UtildHF 
+    rm -f /var/local/kmc/UtildHF
 
     chmod +x /var/local/kmc/UtildSF
     /var/local/kmc/UtildSF
 fi
 
-#Install KPM If Not Already
+#Install KPM 
 if [ ! -x /usr/local/bin/kpm ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/gingrspacecadet/kpm/main/install-kpm.sh)"
   if [ $? -ne 0 ]; then
@@ -38,20 +41,22 @@ if [ ! -x /usr/local/bin/kpm ]; then
   fi
 fi
 
-#Copy App
+#Copy To VAR/LOCAL/MESQUITE
 if [ -d "$SOURCE_DIR" ]; then
-  if [ -d "$TARGET_DIR" ]; then
+    if [ -d "$TARGET_DIR" ]; then
         rm -rf "$TARGET_DIR"
-      fi
-      cp -r "$SOURCE_DIR" "$TARGET_DIR"
-  else
+    fi
+    cp -r "$SOURCE_DIR" "$TARGET_DIR"
+else
     exit 1
 fi
 
-#Register App
+#Redeclare To APPREG.DB 
 sqlite3 "$DB" <<EOF
 INSERT OR IGNORE INTO interfaces(interface) VALUES('application');
+
 INSERT OR IGNORE INTO handlerIds(handlerId) VALUES('$APP_ID');
+
 INSERT OR REPLACE INTO properties(handlerId,name,value) 
   VALUES('$APP_ID','lipcId','$APP_ID');
 INSERT OR REPLACE INTO properties(handlerId,name,value) 
@@ -61,7 +66,7 @@ INSERT OR REPLACE INTO properties(handlerId,name,value)
 EOF
 
 echo Registered $APP_ID, You May Now Launch It With LIPC.
-
-#Run App
 sleep 2
+
+#Launch With LIPC
 nohup lipc-set-prop com.lab126.appmgrd start app://$APP_ID >/dev/null 2>&1 &
