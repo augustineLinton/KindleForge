24bb613384411f297e9137c15f514b83129a33aa
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2025-06-16 21:36:42 +0100

    Quick Test

--- Registry/KindleFetch/install.sh
+++ Registry/KindleFetch/install.sh
@@ -7,9 +7,9 @@ curl https://justrals.github.io/KindleFetch/install.sh | sh
 echo "[KindleForge] KindleFetch Installation Complete."
 
 # Packages.LIST
-FILE="/var/local/mesquite/KindleForge/assets/packages.list"
-STRING="KindleFetch"
+#FILE="/var/local/mesquite/KindleForge/assets/packages.list"
+#STRING="KindleFetch"
 
-grep -qxF "$STRING" "$FILE" || echo "$STRING" >> "$FILE"
+#grep -qxF "$STRING" "$FILE" || echo "$STRING" >> "$FILE"
 
 exit 0
